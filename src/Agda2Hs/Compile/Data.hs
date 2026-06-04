659d26acf208286ac09581ab0265f498457c0400
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2026-06-04 08:12:50 +0200

    Limit telescope upto n if not GADT

--- src/Agda2Hs/Compile/Data.hs
+++ src/Agda2Hs/Compile/Data.hs
@@ -40,7 +40,7 @@ compileData target ds def = do
   let d = hsName $ prettyShow $ qnameName $ defName def
   checkValidTypeName d
   let Datatype{dataPars = n, dataIxs = numIxs, dataCons = cs} = theDef def
-  TelV tel t <- telView {-UpTo n-} (defType def)
+  TelV tel t <- telViewUpTo (if target == ToGadt then -1 else n) (defType def)
   reportSDoc "agda2hs.data" 10 $ text "Datatype telescope:" <+> prettyTCM tel
   --allIndicesErased t
   when (target == ToGadt) $ tellExtension Hs.GADTs
